e9406d79e5bc60f1bc4c1d055c3ea2eb43927cbd
likithakodly516-source <likithakodly516@gmail.com>
2026-07-29 11:08:50 +0530

    HelloWorld.jsx:Creating a ccomponent App.jsx:Importing the component and rendered it

--- Day/Day12/my-react-app/src/App.jsx
+++ Day/Day12/my-react-app/src/App.jsx
@@ -1,11 +1,11 @@
+import { HelloWorld } from "./components/HelloWorld";
 function App () {
 
 
   return (
     <div>
-      <h1>Hello World</h1>
-      <h3>Welcome to my first React App.</h3>
-    </div>
+      <Hello World/>
+     </div>
 
   )
 }
---
+++ Day/Day12/my-react-app/src/components/HelloWorld.jsx
@@ -0,0 +1,10 @@
+ export default function HelloWorld () {
+return (
+    <div>
+      <h1>Hello World</h1>
+      <h3>Welcome to my first React App.</h3>
+    </div>
+ );
+}
+
+export default App
