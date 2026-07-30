67ba395b01227734f76a22f3729c0a22dd8965a3
likithakodly516-source <likithakodly516@gmail.com>
2026-07-30 21:09:36 +0530

    Fetchingdata ex3

--- Day/Day14/my-react-app/src/App.jsx
+++ Day/Day14/my-react-app/src/App.jsx
@@ -1,10 +1,34 @@
 import './App.css'
 import SimpleList from "./components/ListOne";
+import UserList from "./components/ListTwo";
+import ProductListDemo from "./components/ListThree";
+import CounterHook from "./components/HookOne";
+import UserListHook from "./components/HookTwo";
+import ThemeProviderDemo from "./components/HookThree";
+import SimpleNameForm from "./components/FormOne";
+import SignupForm from "./components/FormTwo";
+import ValidatedForm from "./components/FormThree";
+import GitHubUsersList from "./components/FetchingData";
+import GitHubProfile from "./components/FetchingDataTwo";
+import GitHubRepoCount from "./components/FetchingDataThree";
+
 function App() {
   
   return (
     <>
-      <SimpleList />
+      {/* <SimpleList />
+      <UserList />
+      <ProductListDemo />
+
+      <CounterHook/> */}
+      {/* <UserListHook/> */}
+      {/* <ThemeProviderDemo /> */}
+      {/* <SimpleNameForm /> */}
+      {/* <SignupForm /> */}
+      {/* <ValidatedForm/> */}
+      {/* <GitHubUsersList/> */}
+      {/* <GitHubProfile/> */}
+      <GitHubRepoCount username="likithakodly516-source" />
     </>
   )
 }
